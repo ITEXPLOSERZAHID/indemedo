4d903c2d47693c4a52dd5e646f3c803a6b5d9f00
ITEXPLOSERZAHID <itexploserzahid@gmail.com>
2023-01-15 11:04:25 +0500

    UI 9.99%

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:indimedo/APIService.dart';
 import 'package:indimedo/Controller/Maincontrollers.dart';
 import 'package:indimedo/Screen/HomeScreens/PrescriptionScreen.dart';
 import 'package:indimedo/Screen/HomeScreens/SinUpScreen.dart';
@@ -11,6 +12,7 @@ import 'package:indimedo/Testing%20Folder/testcategorydesign.dart';
 
 void main() {
   runApp(const MyApp());
+  apiCall();
   Get.put(MainController()); //used 1 time Get put
 }
 
